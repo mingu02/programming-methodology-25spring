166e0955fb3e1e4b6ca60af81efd63884fa6036a
Mingu <mingu_02@naver.com>
2025-04-11 12:55:29 +0900

    test pass

--- calculator.cpp
+++ calculator.cpp
@@ -1,18 +1,39 @@
 #include "calculator.h"
 #include <stdexcept>
+#include <limits>
 
 int Calculator::add(int a, int b) {
     // TODO
+    if((a > 0 && b > 0 && a + b < 0) || (a < 0 && b < 0 && a + b > 0))
+    {
+        throw std::overflow_error("Overflow");
+    }
+    return a + b;
 }
 
 int Calculator::sub(int a, int b) {
     // TODO
+    if((a > 0 && b < 0 && a - b < 0) || (a < 0 && b > 0 && a - b > 0))
+    {
+        throw std::overflow_error("Overflow");
+    }
+    return a - b;
 }
 
 int Calculator::mul(int a, int b) {
     // TODO
+    if((a > 0 && b > 0 && a * b < 0) || (a < 0 && b < 0 && a * b < 0) || (a < 0 && b > 0 && a * b > 0) || (a > 0 && b < 0 && a * b > 0))
+    {
+        throw std::overflow_error("Overflow");
+    }
+    return a * b;
 }
 
 int Calculator::div(int a, int b) {
     // TODO
+    if(b == 0)
+    {
+        throw std::invalid_argument("Division by zero");
+    }
+    return a / b;
 }
